e437bbf4fe4b1691615c8705757f374a1eaa357a
linjin <1805710616@qq.com>
2019-11-06 00:27:50 +0800

    xiaomi shop

---
+++ xiaomi/pages/home/home/home.js
@@ -0,0 +1,104 @@
+// pages/home/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    src: ['/img/banner1.jpg', '/img/banner2.jpg', '/img/banner3.jpg'],
+    nav: [[{
+      img: "手机.png",
+      text: '手机'
+    },
+    {
+      img: "电视.png",
+      text: '电视'
+    },
+    {
+      img: "电脑.png",
+      text: '电脑'
+    },
+    {
+      img: "智能.png",
+      text: '智能'
+    },
+    {
+      img: "家电-01.png",
+      text: '家电'
+    },], [{
+      img: "1.png",
+      text: '1分平团'
+    },
+    {
+      img: "京东众筹.png",
+      text: '小米众筹'
+    },
+    {
+      img: "秒杀.png",
+      text: '小米秒杀'
+    },
+    {
+      img: "砍价.png",
+      text: '助力砍价'
+    },
+    {
+      img: "红包.png",
+      text: '夺金运动会'
+    }]]
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
